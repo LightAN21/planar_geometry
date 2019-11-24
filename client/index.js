0cf579ca0c123d58206c34fb494a7258c4d9fb4f
Jonathan Tsai <jtsai@e1z2r3p37.42.us.org>
2019-11-23 21:14:21 -0800

    add

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ var curr_tool = 'select';
 var layer = [0];
 var mouse_down = 0;
 var curr_detected_obj = 0;
+var curr_clicked_obj = 0;
 
 console.log('Planar Geometry');
 
@@ -33,15 +34,24 @@ $(document).ready(function () {
         curr_tool = 'circle';
     });
 
-    canvas_div.onmousedown = draw_on_canvas;
-    canvas_div.onmousemove = objects_in_canvas;
+    canvas_div.onmousedown = canvas_mouse_down;
+    canvas_div.onmousemove = canvas_mouse_move;
+    canvas_div.onmouseup = canvas_mouse_up;
 });
 
-function draw_on_canvas(event) {
+function canvas_mouse_down(event) {
     var pos = mouse_pos(canvas_div, event);
 
     console.log('mouse down: (' + pos.x + ', ' + pos.y + ')');
-    if (curr_tool == 'point') {
+    if (curr_tool == 'select') {
+        if (curr_detected_obj != 0 && curr_detected_obj.type == 'point') {
+            var obj = curr_detected_obj;
+            curr_clicked_obj = obj;
+            clear_point(obj.canvas, obj.x, obj.y);
+            draw_point(obj.canvas, obj.x, obj.y, setting.mouse_click_point_color);
+        }
+    }
+    else if (curr_tool == 'point') {
         var new_canvas_box = add_new_point_layer(layer.length);
         var new_canvas = new_canvas_box.getContext("2d")
         canvas_div.appendChild(new_canvas_box);
@@ -55,6 +65,7 @@ function draw_on_canvas(event) {
         });
         draw_point(new_canvas, pos.x, pos.y);
     }
+    mouse_down = 1;
 }
 
 
@@ -83,7 +94,7 @@ function clear_point(ctx, x, y) {
     ctx.clearRect(x - len, y - len, 2 * len, 2 * len);
 }
 
-function objects_in_canvas(event) {
+function canvas_mouse_move(event) {
     var pos = mouse_pos(canvas_div, event);
 
     if (mouse_down == 0) {
@@ -91,7 +102,7 @@ function objects_in_canvas(event) {
         if (document.getElementById('layer1') == undefined)
             return;
         for (var i = 1; i < layer.length; i++) {
-            var imgData = layer[i].canvas.getImageData(pos.x, pos.y, 1, 1).data;
+            var imgData = layer[i].canvas.getImageData(pos.x - 2, pos.y - 2, 1, 1).data;
             if (imgData[0] != 0)
                 top_index = i;
         }
@@ -101,7 +112,7 @@ function objects_in_canvas(event) {
                 clear_point(obj.canvas, obj.x, obj.y);
                 draw_point(obj.canvas, obj.x, obj.y);
             }
-            curr_detected_pbj = 0;
+            curr_detected_obj = 0;
         }
         if (top_index && layer[top_index].type == 'point') {
 
@@ -110,5 +121,35 @@ function objects_in_canvas(event) {
             draw_point(pt.canvas, pt.x, pt.y, setting.mouse_on_point_color);
             curr_detected_obj = pt;
         }
+        else
+            curr_detected_obj = 0;
     }
+    if (mouse_down) {
+        if (curr_clicked_obj) {
+            var obj = curr_clicked_obj;
+            if (obj.type == 'point') {
+                clear_point(obj.canvas, obj.x, obj.y);
+                obj.x = pos.x;
+                obj.y = pos.y;
+                draw_point(obj.canvas, obj.x, obj.y, setting.mouse_click_point_color);
+            }
+        }
+    }
+}
+
+function canvas_mouse_up(event) {
+    var pos = mouse_pos(canvas_div, event);
+
+    console.log('mouse up: (' + pos.x + ', ' + pos.y + ')');
+    if (curr_clicked_obj) {
+        var obj = curr_clicked_obj;
+
+        if (obj.type == 'point') {
+            clear_point(obj.canvas, obj.x, obj.y);
+            draw_point(obj.canvas, obj.x, obj.y, setting.mouse_on_point_color);
+        }
+        curr_clicked_obj = 0;
+        curr_detected_obj = 0;
+    }
+    mouse_down = 0;
 }
--- client/setting.js
+++ client/setting.js
@@ -2,12 +2,13 @@ var setting = {
     canvas_box_background_color: "#424242",
     canvas_width: 500,
     canvas_height: 500,
-    point_radius: 5,
+    point_radius: 8,
     point_lineWidth: 2,
-    clear_point_square_width: 6,
+    clear_point_square_width: 10,
     point_color: "#CCCCCC",
     point_border_color: '#000000',
-    mouse_on_point_color: "#239B56",
+    mouse_on_point_color: "#3498DB",
+    mouse_click_point_color: "#F5B041",
 };
 
 function reset() {
